8db10d68a3f364d7a45ae62748e4963f01036c3e
rara2 <kma952727@gmail.com>
2023-10-10 23:47:52 +0900

    refactor : cors 모두 허용

--- src/main/java/com/example/demo/global/config/WebConfig.java
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -26,8 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3000);
     }
