30ac60e28db7066987475a29389de8b3dffe672c
rara2 <kma952727@gmail.com>
2023-10-13 01:07:34 +0900

    featrue : 회원가입 요청 조건 추가

--- src/main/java/com/example/demo/domain/member/controller/dto/SignUpRequest.java
+++ src/main/java/com/example/demo/domain/member/controller/dto/SignUpRequest.java
@@ -8,21 +8,15 @@ import jakarta.validation.constraints.Size;
 @Schema
 public record SignUpRequest (
         @Schema(description = "서비스에서 사용하는 아이디")
-        @Size(max = 10, message = "사용자 아이디 : 10자리 이하")
-        @NotBlank(message = "사용자 아이디 : 필수 값")
+        @Size(min=6, max = 20, message = "사용자 아이디 : 6자리 이상, 10자리 이하")
         String serviceId,
 
-        @Schema(description = "사용자 이메일")
-        @NotBlank(message = "이메일 : 필수 값")
-        @Email
-        String email,
-
         @Schema(description = "사용자 닉네임")
-        @NotBlank(message = "별칭: 필수 값")
+        @Size(min = 2, max = 10, message = "사용자 닉네임 : 2자리 이상, 10자리 이하")
         String nickname,
 
         @Schema(description = "암호")
-        @NotBlank(message = "암호: 필수 값")
+        @Size(min = 6, message = "사용자 암호 : 6자리 이상")
         String password
 ) {
 
