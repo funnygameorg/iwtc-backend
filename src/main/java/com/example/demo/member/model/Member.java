849b93ccc170804c1c52a19817e70bda6760c68b
rara2 <kma952727@gmail.com>
2023-10-17 00:29:41 +0900

    feature: Memeber class equals hashcode 오버라이드

--- src/main/java/com/example/demo/member/model/Member.java
+++ src/main/java/com/example/demo/member/model/Member.java
@@ -3,6 +3,9 @@ package com.example.demo.member.model;
 import com.example.demo.common.jpa.TimeBaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -18,6 +21,20 @@ public class Member extends TimeBaseEntity {
     private String nickname;
 
     private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.intValue();
+    }
 
     @Builder
     public Member(Long id, String serviceId, String nickname, String password) {
