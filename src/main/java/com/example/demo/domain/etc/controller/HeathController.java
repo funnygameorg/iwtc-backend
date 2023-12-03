ee65a7791d9f6321266aa6dc79adb97defa8e502
rara2 <kma952727@gmail.com>
2023-12-03 16:47:42 +0900

    refactor: 헬스체크 api 변경

--- src/main/java/com/example/demo/domain/etc/controller/HeathController.java
+++ src/main/java/com/example/demo/domain/etc/controller/HeathController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HeathController {
 
-    @GetMapping("/health")
+    @GetMapping("/")
     public String getHealth() {
         return "OK";
     }
