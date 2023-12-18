7c0de21b22843df92e80b936dd1f0731ac8d0d3d
rara2 <kma952727@gmail.com>
2023-12-18 15:51:56 +0900

    refactor : S3 Local, Test 환경에서 사용하는 컨디션 클래스 작성

--- src/main/java/com/example/demo/common/config/s3/LocalS3Config.java
+++ src/main/java/com/example/demo/common/config/s3/LocalS3Config.java
@@ -5,10 +5,9 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -16,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
     localstack S3 연결
  */
 @Configuration
-@ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local")
+@Conditional(NotProdS3ConfigCondition.class)
 public class LocalS3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
