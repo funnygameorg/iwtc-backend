f1770dc8b23cd1b43d3a52ca8a0f61c7cc3b46c4
rara2 <kma952727@gmail.com>
2024-02-16 13:43:33 +0900

    refactor : mybatis mapper xml 코드 정리
    1. @Alias 적용
    2. SQL 정리 + 주석 작성

--- src/main/java/com/masikga/itwc/domain/worldcup/repository/projection/GetDividedWorldCupGameContentsProjection.java
+++ src/main/java/com/masikga/itwc/domain/worldcup/repository/projection/GetDividedWorldCupGameContentsProjection.java
@@ -1,11 +1,14 @@
 package com.masikga.itwc.domain.worldcup.repository.projection;
 
+import org.apache.ibatis.type.Alias;
+
+@Alias("GetDividedWorldCupGameContentsProjection")
 public record GetDividedWorldCupGameContentsProjection(
-	long contentsId,
-	String name,
-	long mediaFileId,
-	String FileType,
-	String movieStartTime,
-	Integer moviePlayDuration
+        long contentsId,
+        String name,
+        long mediaFileId,
+        String FileType,
+        String movieStartTime,
+        Integer moviePlayDuration
 ) {
 }
