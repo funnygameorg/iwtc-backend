a814b2976aebb4f1b4b8ba26f8746c13a98c5b07
rara2 <kma952727@gmail.com>
2024-03-20 23:27:50 +0900

    feature : db module @SpringBootApplication 추가,

---
+++ core/db/src/main/java/com/masikga/db/ItwcApplication.java
@@ -0,0 +1,13 @@
+package com.masikga.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.masikga")
+public class ItwcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItwcApplication.class, args);
+    }
+
+}
