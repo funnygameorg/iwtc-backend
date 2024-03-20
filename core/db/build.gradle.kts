7a49a8835297449003742555202ee3c82c100cc6
rara2 <kma952727@gmail.com>
2024-03-21 00:31:23 +0900

    feature : db module boot starter jdbc 추가

--- core/db/build.gradle.kts
+++ core/db/build.gradle.kts
@@ -19,15 +19,8 @@ configurations {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.3")
     implementation("mysql:mysql-connector-java:8.0.33")
-
-    implementation("org.springframework.security:spring-security-crypto")
-    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
-    
-    // jwt
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
