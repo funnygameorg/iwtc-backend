2527b9abe79b8d2cedd2ed4007841aeda0a301bf
rara2 <kma952727@gmail.com>
2024-03-27 08:40:10 +0900

    feature : db.yml 수정

---
+++ service/member/src/main/java/com/masikga/member/infra/rememberme/impl/rdb/BlackAccessToken.java
@@ -0,0 +1,26 @@
+package com.masikga.member.infra.rememberme.impl.rdb;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "black_access_token")
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlackAccessToken {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String accessToken;
+
+    private LocalDateTime registeredAt;
+}
--- service/member/src/main/java/com/masikga/member/infra/rememberme/impl/rdb/RDBRememberMe.java
+++ service/member/src/main/java/com/masikga/member/infra/rememberme/impl/rdb/RDBRememberMe.java
@@ -3,6 +3,7 @@ package com.masikga.member.infra.rememberme.impl.rdb;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -14,25 +15,26 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Builder
+@Table(name = "rdbremember_me")
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class RDBRememberMe {
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
-	private Long memberId;
+    private Long memberId;
 
-	private LocalDateTime rememberAt;
+    private LocalDateTime rememberAt;
 
-	// 로그인 최종 유지시간이 만료되었는가?
-	public boolean isExpired(LocalDateTime now, Long refreshTokenValiditySeconds) {
+    // 로그인 최종 유지시간이 만료되었는가?
+    public boolean isExpired(LocalDateTime now, Long refreshTokenValiditySeconds) {
 
-		var expiredTime = rememberAt.plusSeconds(refreshTokenValiditySeconds / 1000);
-		return !expiredTime.isAfter(now);
+        var expiredTime = rememberAt.plusSeconds(refreshTokenValiditySeconds / 1000);
+        return !expiredTime.isAfter(now);
 
-	}
+    }
 
 }
